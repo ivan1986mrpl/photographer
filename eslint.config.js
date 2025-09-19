ae602822685e8cab0d5b9ce9ace14e3600464138
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-09-19 17:44:02 +0300

    init: start new project

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const ignores = [
+  '**/node_modules/**',
+  '**/dist/**',
+  '**/build/**',
+  '**/coverage/**',
+  '**/*.log',
+  '**/npm-debug.log*',
+  '**/.git/**',
+  '**/.idea/**',
+  '**/.vscode/**',
+  '**/.DS_Store',
+  'postcss.config.js',
+  'vite.config.js',
+];
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    ignores,
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'no-console': 'warn',
+      eqeqeq: 'warn',
+      curly: 'warn',
+      'no-else-return': 'warn',
+      'no-unused-vars': 'warn',
+    },
+  },
+]);
---
+++ vite.config.js
@@ -0,0 +1,56 @@
+import SassGlob from 'vite-plugin-sass-glob-import';
+import { defineConfig } from 'vite';
+import { sync } from 'glob';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+const noAttr = () => {
+  return {
+    transformIndexHtml(html) {
+      return html.replaceAll(' crossorigin', '');
+    },
+  };
+};
+
+export default defineConfig({
+  plugins: [
+    SassGlob(),
+    noAttr(),
+    ViteImageOptimizer({
+      // https://sharp.pixelplumbing.com/api-output#jpeg
+      //https://github.com/FatehAK/vite-plugin-image-optimizer
+      png: {
+        quality: 70,
+      },
+      jpeg: {
+        quality: 70,
+      },
+      jpg: {
+        quality: 70,
+      },
+      webp: {
+        quality: 85,
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: sync('src/**/*.html'.replace(/\\/g, '/')),
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name;
+          if (/css/.test(extType)) {
+            extType = 'assets/css';
+          }
+          return assetInfo.originalFileName ?? `${extType}/[name][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+    },
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    outDir: '../dist',
+  },
+  root: 'src',
+  base: '',
+});
